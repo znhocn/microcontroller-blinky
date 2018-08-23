67ca5c6e3bbd0c800c2379004d4909db40077c8b
ZnHoCn <me@hakr.xyz>
2018-08-23 23:26:05 +0800

    add ATtiny85 Blink

--- Atmel AVR/ATtiny85/Blink/Blink.ino
+++ Atmel AVR/ATtiny85/Blink/Blink.ino
@@ -7,7 +7,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
+  delay(1000);                       // wait for a second
   digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
