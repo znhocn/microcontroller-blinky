943c12b0bac9cb4d11b8328259a66ca84e98026f
ZnHoCn <me@hakr.xyz>
2018-08-26 11:35:45 +0800

    Add ATmega32U4 Blink

---
+++ Atmel AVR/ATmega32U4/PlatformIO/Blink/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(3, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                       // wait for a second
+    digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
+    delay(100);                       // wait for a second
+}
