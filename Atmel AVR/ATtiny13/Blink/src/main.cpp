985407ddc819fd4e3732e382d154f6220dca1720
ZnHoCn <me@hakr.xyz>
2018-08-27 03:09:12 +0800

    Add ATtiny13 Blink

---
+++ Atmel AVR/ATtiny13/Blink/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+int ledPin = 4;
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);                       // wait for a second
+    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+    delay(100);                       // wait for a second
+}
