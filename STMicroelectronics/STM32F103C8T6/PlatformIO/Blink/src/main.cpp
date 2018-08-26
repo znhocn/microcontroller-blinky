2584b1c39417e68f9d6ab2c29dabcfe109fd3d0d
ZnHoCn <me@hakr.xyz>
2018-08-26 13:29:27 +0800

    Add STM32F103C8T6 Blink

--- STMicroelectronics/STM32F103C8T6/PlatformIO/Blink/src/main.cpp
+++ STMicroelectronics/STM32F103C8T6/PlatformIO/Blink/src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
     // put your main code here, to run repeatedly:
     digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
+    delay(100);                       // wait for a second
     digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+    delay(100);                       // wait for a second
 }
