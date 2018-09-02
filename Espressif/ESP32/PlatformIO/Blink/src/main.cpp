b1ab8053296c5a476213d3121b01545924982fce
ZnHoCn <me@hakr.xyz>
2018-09-02 19:23:52 +0800

    Add ESP32 Blink

--- Espressif/ESP32/PlatformIO/Blink/src/main.cpp
+++ Espressif/ESP32/PlatformIO/Blink/src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 
+int ledPin = 4;
+
 void setup() {
     // put your setup code here, to run once:
-    pinMode(4, OUTPUT);    // Pin #4 is NodeMCU-32S GPIO4 D4
+    pinMode(ledPin, OUTPUT);    // Pin #4 is NodeMCU-32S GPIO4 D4
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(100);                       // wait for a second
-    digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
-    delay(100);                       // wait for a second
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                       // wait for a second
 }
